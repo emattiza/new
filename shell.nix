60c9d22d7636aaf15230cb8a8e9643eb94d70902
Evan Mattiza <emattiza@gmail.com>
2023-12-06 05:29:31 +0000

    more shell.nix changes

--- shell.nix
+++ shell.nix
@@ -1,21 +1,13 @@
-# https://nix.dev/tutorials/declarative-and-reproducible-developer-environments
 with import <nixpkgs> { };
-
 mkShell {
-
-  # Package names can be found via https://search.nixos.org/packages
   nativeBuildInputs = [
-    # baseline 
     direnv
     gitFull
     subversion
     eza
   ];
-
   NIX_ENFORCE_PURITY = true;
-
   shellHook = ''
-  alias tf=terraform
-  alias lse='exa -lah'
+    alias lse='eza -lah'
   '';
 }
